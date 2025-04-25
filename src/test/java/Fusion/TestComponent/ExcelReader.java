b7449fa7f4f236816e02d34b94f5ac9f2acbdb54
bpanchal683 <bpanchal@itconvergence.com>
2025-04-25 12:05:10 +0530

    Excel second Row

--- src/test/java/Fusion/TestComponent/ExcelReader.java
+++ src/test/java/Fusion/TestComponent/ExcelReader.java
@@ -13,45 +13,45 @@ import java.util.Map;
 public class ExcelReader {
 
     //Code for Single set of data
-//    public static List<Map<String, String>> getTestData(String filePath, String sheetName) throws IOException {
-//        FileInputStream fis = new FileInputStream(filePath);
-//        Workbook workbook = new XSSFWorkbook(fis);
-//        Sheet sheet = workbook.getSheet(sheetName);
-//
-//        List<Map<String, String>> dataList = new ArrayList<>();
-//        Row headerRow = sheet.getRow(0);
-//        int rowCount = sheet.getPhysicalNumberOfRows();
-//        int colCount = headerRow.getLastCellNum();
-//
-//        for (int i = 1; i < rowCount; i++) {
-//            Row row = sheet.getRow(i);
-//            Map<String, String> dataMap = new HashMap<>();
-//            for (int j = 0; j < colCount; j++) {
-//                String key = headerRow.getCell(j).getStringCellValue();
-//                String value = "";
-//                Cell cell = row.getCell(j);
-//                if (cell != null) {
-//                    value = switch (cell.getCellType()) {
-//                        case STRING -> cell.getStringCellValue();
-//                        case NUMERIC -> DateUtil.isCellDateFormatted(cell)
-//                                ? cell.getDateCellValue().toString()
-//                                : String.valueOf(cell.getNumericCellValue());
-//                        case BOOLEAN -> String.valueOf(cell.getBooleanCellValue());
-//                        default -> "";
-//                    };
-//                }
-//                dataMap.put(key, value);
-//            }
-//            dataList.add(dataMap);
-//        }
-//
-//        workbook.close();
-//        fis.close();
-//        return dataList;
-//    }
+    public static List<Map<String, String>> getTestData(String filePath, String sheetName) throws IOException {
+        FileInputStream fis = new FileInputStream(filePath);
+        Workbook workbook = new XSSFWorkbook(fis);
+        Sheet sheet = workbook.getSheet(sheetName);
+
+        List<Map<String, String>> dataList = new ArrayList<>();
+        Row headerRow = sheet.getRow(0);
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        int colCount = headerRow.getLastCellNum();
+
+        for (int i = 1; i < rowCount; i++) {
+            Row row = sheet.getRow(i);
+            Map<String, String> dataMap = new HashMap<>();
+            for (int j = 0; j < colCount; j++) {
+                String key = headerRow.getCell(j).getStringCellValue();
+                String value = "";
+                Cell cell = row.getCell(j);
+                if (cell != null) {
+                    value = switch (cell.getCellType()) {
+                        case STRING -> cell.getStringCellValue();
+                        case NUMERIC -> DateUtil.isCellDateFormatted(cell)
+                                ? cell.getDateCellValue().toString()
+                                : String.valueOf(cell.getNumericCellValue());
+                        case BOOLEAN -> String.valueOf(cell.getBooleanCellValue());
+                        default -> "";
+                    };
+                }
+                dataMap.put(key, value);
+            }
+            dataList.add(dataMap);
+        }
+
+        workbook.close();
+        fis.close();
+        return dataList;
+    }
 
     //code for multiple set of data
-    public static List<Map<String, String>> getTestData(String filePath, String sheetName) throws IOException {
+    public static List<Map<String, String>> getMultiTestData(String filePath, String sheetName) throws IOException {
         List<Map<String, String>> dataList = new ArrayList<>();
 
         FileInputStream fis = new FileInputStream(filePath);
